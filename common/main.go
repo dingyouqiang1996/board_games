2846eec8a620188b23d60831fbbedf94f624b1d3
dingyouqiang1996 <128674406+dingyouqiang1996@users.noreply.github.com>
2024-12-05 06:18:43 +0000

    日志

--- common/main.go
+++ common/main.go
@@ -12,5 +12,13 @@ func main() {
 	logs.InitLog("test")
 	log.Info("Hello, World!")
 	log.Info("Hello, World!")
+	log.Debug("Hello, World!") // 这里我的调用没有按照课程的来，所以等级为info，打印不出debug的日志
+	// log.Fatal("Hello, World!")
+	log.Error("Hello, World!")
 	log.Info("Hello, World!")
+	a := 1
+	go func() {
+		log.Info(a)
+	}()
+	select {}
 }
